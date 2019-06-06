aa01f1db03f8a11498c6e14a1e29630c797ec46d
xiangyu <xiangyu@localhost.com>
2019-06-06 15:06:39 +0800

    添加权限验证模块！！！！

---
+++ blog-asd/src/main/java/com/jackie/blogasd/util/ShiroConfig.java
@@ -0,0 +1,5 @@
+package com.jackie.blogasd.util;
+
+public class ShiroConfig {
+
+}
--- blog-asd/src/main/java/com/jackie/blogasd/util/ShiroRealm.java
+++ blog-asd/src/main/java/com/jackie/blogasd/util/ShiroRealm.java
@@ -5,6 +5,7 @@ import com.jackie.blogasd.model.SysUser;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -27,6 +28,7 @@ public class ShiroRealm extends AuthorizingRealm {
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token= (UsernamePasswordToken) authenticationToken;
         return null;
     }
 }
