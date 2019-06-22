4b669aea4f20f6aecc8c0b0031c4f4425d354b18
xiangyu <xiangyu@localhost.com>
2019-06-22 10:41:00 +0800

    init blog-handler commit

---
+++ blog-handle/src/main/java/com/jackie/bloghandle/BlogHandleApplication.java
@@ -0,0 +1,13 @@
+package com.jackie.bloghandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogHandleApplication.class, args);
+    }
+
+}
---
+++ blog-handle/src/test/java/com/jackie/bloghandle/BlogHandleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jackie.bloghandle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogHandleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
