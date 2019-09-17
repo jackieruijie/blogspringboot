d82817cffcfee5884e8e70aaab6e077e43087085
xiangyu <jackiexiangxy@gmail.com>
2019-09-17 23:06:58 +0800

    create blog-amqp and init project

---
+++ blog-amqp/src/main/java/com/jackie/blogamqp/BlogAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.jackie.blogamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAmqpApplication.class, args);
+    }
+
+}
