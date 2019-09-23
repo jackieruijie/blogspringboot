222636c0296e14ba3e1d8a58924f3a3f5d602baa
xiangyu <xiangyu@localhost.com>
2019-09-23 14:49:52 +0800

    添加blog admin 服务

---
+++ blog-admin/src/test/java/com/jackie/blogadmin/BlogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackie.blogadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
